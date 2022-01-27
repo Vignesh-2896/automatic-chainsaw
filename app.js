b1cca9ee13f9a56696fda634d0eabcaa9a18ca77
Vignesh B <vigneshb2896@gmail.com>
2022-01-27 14:03:03 +0530

    Heroku Deployment.

--- app.js
+++ app.js
@@ -13,9 +13,8 @@ var app = express();
 
 //Database Setup
 
-let mongoDBURL = "";
 var mongoose = require('mongoose');
-var mongoDB = mongoDBURL;
+var mongoDB = 'mongodb+srv://m001-student:m001-mongodb-basics@cluster0.y9qpd.mongodb.net/InventoryApp?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
--- routes/haikyuu.js
+++ routes/haikyuu.js
@@ -22,11 +22,9 @@ var storage = multer.diskStorage({
 
 var upload = multer({storage : storage});
 
-var yourPassword = "";
-
 function myAuthorizer(username, password) { // Basic password auth using express-basic-auth.
   const userMatches = basicAuth.safeCompare(username, 'admin')
-  const passwordMatches = basicAuth.safeCompare(password, yourPassword)
+  const passwordMatches = basicAuth.safeCompare(password, '#10ark$$b@T')
 
   return userMatches & passwordMatches
 }
